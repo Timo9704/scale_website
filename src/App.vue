a7897d8addb270027516a9ff71f5d0fc4047ae0c
Timo9704 <51120668+Timo9704@users.noreply.github.com>
2023-05-22 21:28:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <HelloWorld msg="Scale + Vue.js"/>
+  </main>
+</template>
+
+<script>
+import HelloWorld from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <header class="header">
+        <img v-bind:src="'/assets/logo.png'"/>
+        <nav-buttons>
+            <scale-button class="nav-button">Über mich</scale-button>
+            <scale-button class="nav-button">Meine Aquarien</scale-button>
+            <scale-button class="nav-button">Impressum</scale-button>
+        </nav-buttons>
+    </header>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.header{
+    position: absolute;
+    right: 70px;
+    left: 40px;
+    top: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.nav-button{
+    color: white;
+    font-size: 30px;
+    margin-left: 40px;
+}
+
+</style>
